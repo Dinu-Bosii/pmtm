e79831aa5fdce07adf18a0278a1c9ca667d0e4eb
Dinu-Bosii <knsverts@gmail.com>
2023-09-06 16:53:15 +0100

    Add Js and html do dynamically load content

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const { invoke } = window.__TAURI__.tauri;
 
 const menuButton = document.querySelector('.menu-icon');
 const menuSidebar = document.querySelector('.menu-sidebar');
-console.log('This is a regular console.log message');
+
 menuButton.addEventListener('click', () => {
 
     if(menuSidebar.style.display === 'block') {
@@ -12,4 +12,39 @@ menuButton.addEventListener('click', () => {
         menuSidebar.style.display = 'block';
     }
 });
+fetchPage('pages/home.html');
+
+document.getElementById('timer-item')
+        .addEventListener('click', () => {
+            fetchPage('/pages/timer.html');
+        });
+
+document.getElementById('home-item')
+        .addEventListener('click', () => {
+            fetchPage('/pages/home.html');
+        });
+
+
+
+function fetchPage(page) {
+    fetch(page)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Failed to load content');
+            }
+            return response.text();
+        })
+        .then((pageContent) => {
+            console.log("Fetching timer page...")
+            document.querySelector('.content')
+                .innerHTML = pageContent;
+            console.log("Timer page obtained.");
+            menuSidebar.style.display = 'none';
+        })
+        .catch((error) => {
+            console.error('Error loading content:', error);
+        });
+}
+        
+
 
